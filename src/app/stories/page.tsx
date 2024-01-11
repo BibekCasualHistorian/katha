c35efbfdf10fa54c6b66bd8093e8658b32ba6164
Bibek Koirala <www.koiralabibek2058@gmail.com>
2024-01-11 21:40:02 +0545

    really

---
+++ src/app/stories/page.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div className="grid grid-cols-4 px-8 p-5 gap-7 text-white">
+      <div
+        className="text-white  rounded-xl p-5"
+        style={{ background: "#343334" }}
+      >
+        <h1 className="text-2xl mb-2">Update Password</h1>
+        <p className="mb-3 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ut
+          laudantium. Natus?
+        </p>
+        <address className="mb-1">12/02/2023</address>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green rounded-3xl text-white py-1 px-3 bg-green-600">
+            Completed
+          </h3>
+          <p className="text-2xl">➡️</p>
+        </div>
+      </div>
+      <div
+        className="text-white  rounded-xl p-5"
+        style={{ background: "#343334" }}
+      >
+        <h1 className="text-2xl mb-2">Update Password</h1>
+        <p className="mb-3 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ut
+          laudantium. Natus?
+        </p>
+        <address className="mb-1">12/02/2023</address>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green rounded-3xl text-white py-1 px-3 bg-green-600">
+            Completed
+          </h3>
+          <p className="text-2xl">➡️</p>
+        </div>
+      </div>
+      <div
+        className="text-white  rounded-xl p-5"
+        style={{ background: "#343334" }}
+      >
+        <h1 className="text-2xl mb-2">Update Password</h1>
+        <p className="mb-3 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ut
+          laudantium. Natus?
+        </p>
+        <address className="mb-1">12/02/2023</address>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green rounded-3xl text-white py-1 px-3 bg-green-600">
+            Completed
+          </h3>
+          <p className="text-2xl">➡️</p>
+        </div>
+      </div>
+      <div
+        className="text-white  rounded-xl p-5"
+        style={{ background: "#343334" }}
+      >
+        <h1 className="text-2xl mb-2">Update Password</h1>
+        <p className="mb-3 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ut
+          laudantium. Natus?
+        </p>
+        <address className="mb-1">12/02/2023</address>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green rounded-3xl text-white py-1 px-3 bg-green-600">
+            Completed
+          </h3>
+          <p className="text-2xl">➡️</p>
+        </div>
+      </div>
+      <div
+        className="text-white  rounded-xl p-5"
+        style={{ background: "#343334" }}
+      >
+        <h1 className="text-2xl mb-2">Update Password</h1>
+        <p className="mb-3 text-sm">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum, ut
+          laudantium. Natus?
+        </p>
+        <address className="mb-1">12/02/2023</address>
+        <div className="flex justify-between items-center">
+          <h3 className="bg-green rounded-3xl text-white py-1 px-3 bg-green-600">
+            Completed
+          </h3>
+          <p className="text-2xl">➡️</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,19 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 const Header = () => {
+  const pathNameRoute = usePathname();
+
+  const normalLinks = [
+    { pathName: "/shorts", title: "Shorts" },
+    { pathName: "/series", title: "Series" },
+    { pathName: "/stories", title: "Stories" },
+    { pathName: "/wishlist", title: "WishList" },
+    { pathName: "/reading", title: "Reading" },
+  ];
   return (
     <header className="text-white flex justify-evenly p-5 items-center">
       <div className="flex flex-col w-8 gap-1">
@@ -9,11 +21,20 @@ const Header = () => {
         <span className="w-full h-1 bg-white rounded-md"></span>
         <span className="w-full h-1 bg-white rounded-md"></span>
       </div>
-      <Link href={""}>Shorts</Link>
-      <Link href={""}>Series</Link>
-      <Link href={""}>Stories</Link>
-      <Link href={""}>WishList</Link>
-      <Link href={""}>Reading</Link>
+      {normalLinks.map((each) => {
+        return (
+          <Link
+            href={each.pathName}
+            className={`${
+              each.pathName == pathNameRoute
+                ? "font-bold text-white"
+                : "text-gray-300"
+            }`}
+          >
+            {each.title}
+          </Link>
+        );
+      })}
       <Link href={""} className="w-10 bg-white rounded-full h-10 block"></Link>
     </header>
   );
